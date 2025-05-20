038935f0861c999ec53e65526bf8cd5a51a89926
IceUniverse6 <ali84azizi2005@gmail.com>
2025-05-20 21:07:09 +0330

    artist class
    هنوز تکمیل نشده...

--- MusicPlayer/MusicPlayer/Artist.h
+++ MusicPlayer/MusicPlayer/Artist.h
@@ -1,5 +1,29 @@
 #pragma once
+#include<iostream>
+#include<vector>
+#include"Music.h"
 class Artist
 {
+private:
+	std::string artist_name;
+	int number_of_albums;
+	int number_of_released_songs;
+	std::vector<Music> released_songs_list;
+public:
+	Artist();
+	Artist(std::string, int , int);
+	~Artist();
+
+	void set_artist_name(std::string);
+	void set_number_of_albums(int);
+	void set_number_of_released_songs(int);
+
+	void add_song();
+	void remove_song();
+
+	std::string get_artist_name()const;
+	int get_number_of_albums()const;
+	int get_number_of_released_songs()const;
+	std::vector<Music> get_all_songs()const;
 };
 
