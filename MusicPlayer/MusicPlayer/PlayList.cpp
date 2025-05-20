bb032a5e5d2b4113d1ef9e6a9ce6cbb37ae9694c
IceUniverse6 <ali84azizi2005@gmail.com>
2025-05-20 20:52:52 +0330

    playlist class
    playlist class was written

---
+++ MusicPlayer/MusicPlayer/PlayList.cpp
@@ -0,0 +1,40 @@
+#include "PlayList.h"
+
+PlayList::PlayList() {
+
+}
+PlayList::PlayList(std::string play_list_name) {
+	this->play_list_name = play_list_name;
+}
+PlayList::~PlayList() {
+
+}
+
+void PlayList::set_play_list_name(std::string play_list_name) {
+	this->play_list_name = play_list_name;
+}
+void PlayList::add_music(Music music) {
+	this->music_list.push_back(music);
+}
+void PlayList::remove_music(int index) {
+	this->music_list.erase(this->music_list.begin() + index);
+}
+
+std::string PlayList::get_play_list_name()const {
+	return this->play_list_name;
+}
+int PlayList::get_number_of_songs()const {
+	return this->music_list.size();
+}
+std::vector<Music> PlayList::get_all_songs()const {
+	return this->music_list;
+}
+
+void PlayList::show_songs()const {
+	for (size_t i = 0; i < this->music_list.size(); i++) {
+		std::cout << this->music_list.at(i).get_music_name() << std::endl;
+		std::cout << this->music_list.at(i).get_artist_name() << std::endl;
+		std::cout << this->music_list.at(i).get_genre() << std::endl;
+		std::cout << this->music_list.at(i).get_release_year() << std::endl;
+	}
+}
--- MusicPlayer/MusicPlayer/PlayList.h
+++ MusicPlayer/MusicPlayer/PlayList.h
@@ -1,5 +1,24 @@
 #pragma once
+#include<vector>
+#include"Music.h"
 class PlayList
 {
+private:
+	std::string play_list_name;
+	std::vector<Music> music_list;
+public:
+	PlayList();
+	PlayList(std::string);
+	~PlayList();
+
+	void set_play_list_name(std::string);
+	void add_music(Music);
+	void remove_music(int);
+
+	std::string get_play_list_name()const;
+	int get_number_of_songs()const;
+	std::vector<Music> get_all_songs()const;
+
+	void show_songs()const;
 };
 
