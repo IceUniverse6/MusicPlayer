54815ac3868f6ca79e90c0c35c04868b6394de10
IceUniverse6 <ali84azizi2005@gmail.com>
2025-05-22 08:09:36 +0330

    class play list
    تغییر نوع وکتور و اضافه کردن تابع نمایش اطلاعات

--- MusicPlayer/MusicPlayer/PlayList.cpp
+++ MusicPlayer/MusicPlayer/PlayList.cpp
@@ -13,7 +13,7 @@ PlayList::~PlayList() {
 void PlayList::set_play_list_name(std::string play_list_name) {
 	this->play_list_name = play_list_name;
 }
-void PlayList::add_music(Music music) {
+void PlayList::add_music(std::shared_ptr<Music> music) {
 	this->music_list.push_back(music);
 }
 void PlayList::remove_music(int index) {
@@ -26,15 +26,19 @@ std::string PlayList::get_play_list_name()const {
 int PlayList::get_number_of_songs()const {
 	return this->music_list.size();
 }
-std::vector<Music> PlayList::get_all_songs()const {
+std::vector<std::shared_ptr<Music>> PlayList::get_all_songs()const {
 	return this->music_list;
 }
 
 void PlayList::show_songs()const {
 	for (size_t i = 0; i < this->music_list.size(); i++) {
-		std::cout << this->music_list.at(i).get_music_name() << std::endl;
-		std::cout << this->music_list.at(i).get_artist_name() << std::endl;
-		std::cout << this->music_list.at(i).get_genre() << std::endl;
-		std::cout << this->music_list.at(i).get_release_year() << std::endl;
+		std::cout << this->music_list.at(i)->get_music_name() << std::endl;
+		std::cout << this->music_list.at(i)->get_artist_name() << std::endl;
+		std::cout << this->music_list.at(i)->get_genre() << std::endl;
+		std::cout << this->music_list.at(i)->get_release_year() << std::endl;
 	}
+}
+void PlayList::show_play_list_info()const {
+	std::cout << "play list name: " << this->play_list_name << std::endl;
+	std::cout << "number of songs: " << this->get_number_of_songs() << std::endl;
 }
--- MusicPlayer/MusicPlayer/PlayList.h
+++ MusicPlayer/MusicPlayer/PlayList.h
@@ -1,25 +1,27 @@
 #pragma once
 #include<iostream>
 #include<vector>
+#include<memory>
 #include"Music.h"
 class PlayList
 {
 private:
 	std::string play_list_name;
-	std::vector<Music> music_list;
+	std::vector<std::shared_ptr<Music>> music_list;
 public:
 	PlayList();
 	PlayList(std::string);
 	~PlayList();
 
 	void set_play_list_name(std::string);
-	void add_music(Music);
+	void add_music(std::shared_ptr<Music>);
 	void remove_music(int);
 
 	std::string get_play_list_name()const;
 	int get_number_of_songs()const;
-	std::vector<Music> get_all_songs()const;
+	std::vector<std::shared_ptr<Music>> get_all_songs()const;
 
 	void show_songs()const;
+	void show_play_list_info()const;
 };
 
