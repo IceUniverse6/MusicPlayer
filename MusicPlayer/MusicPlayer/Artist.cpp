1e943365cd703484c943c6411b3912db2dc098bc
IceUniverse6 <ali84azizi2005@gmail.com>
2025-05-22 08:27:37 +0330

    class artist
    تغییر نوع وکتور و اضافه کردن توابع افزودن یا حذف پلی لیست و نمایش اطلاعات خواننده

--- MusicPlayer/MusicPlayer/Artist.cpp
+++ MusicPlayer/MusicPlayer/Artist.cpp
@@ -23,11 +23,19 @@ void Artist::set_number_of_released_songs(int number_of_released_songs) {
 	this->number_of_released_songs = number_of_released_songs;
 }
 
-void Artist::add_song(Music music) {
+void Artist::add_song(std::shared_ptr<Music> music) {
 	this->released_songs_list.push_back(music);
 }
 void Artist::remove_song(int index) {
 	this->released_songs_list.erase(this->released_songs_list.begin() + index);
+}
+
+void Artist::add_play_list(std::shared_ptr<PlayList> play_list) {
+	this->artist_play_lists.push_back(play_list);
+}
+void Artist::remove_play_list(int index) {
+	this->artist_play_lists.erase(this->artist_play_lists.begin() + index);
+
 }
 
 std::string Artist::get_artist_name()const {
@@ -39,6 +47,12 @@ int Artist::get_number_of_albums()const {
 int Artist::get_number_of_released_songs()const {
 	return this->number_of_released_songs;
 }
-std::vector<Music> Artist::get_all_songs()const {
+std::vector<std::shared_ptr<Music>> Artist::get_all_songs()const {
 	return this->released_songs_list;
+}
+
+void Artist::show_artist_info()const {
+	std::cout << "artist name: " << this->artist_name << std::endl;
+	std::cout << "number of albums: " << this->number_of_albums << std::endl;
+	std::cout << "number of released songs: " << this->number_of_released_songs << std::endl;
 }
--- MusicPlayer/MusicPlayer/Artist.h
+++ MusicPlayer/MusicPlayer/Artist.h
@@ -1,14 +1,17 @@
 #pragma once
 #include<iostream>
 #include<vector>
+#include<memory>
 #include"Music.h"
+#include"PlayList.h"
 class Artist
 {
 private:
 	std::string artist_name;
 	int number_of_albums;
 	int number_of_released_songs;
-	std::vector<Music> released_songs_list;
+	std::vector<std::shared_ptr<Music>> released_songs_list;
+	std::vector<std::shared_ptr<PlayList>> artist_play_lists;
 public:
 	Artist();
 	Artist(std::string, int , int);
@@ -18,12 +21,17 @@ public:
 	void set_number_of_albums(int);
 	void set_number_of_released_songs(int);
 
-	void add_song(Music);
+	void add_song(std::shared_ptr<Music>);
 	void remove_song(int);
+
+	void add_play_list(std::shared_ptr<PlayList>);
+	void remove_play_list(int);
 
 	std::string get_artist_name()const;
 	int get_number_of_albums()const;
 	int get_number_of_released_songs()const;
-	std::vector<Music> get_all_songs()const;
+	std::vector<std::shared_ptr<Music>> get_all_songs()const;
+
+	void show_artist_info()const;
 };
 
